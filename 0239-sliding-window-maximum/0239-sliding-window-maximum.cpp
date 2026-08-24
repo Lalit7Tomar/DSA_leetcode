9bbc0021858caee9f4888a69bca142889dc43b23
Lalit7Tomar <lalittomar0612@gmail.com>
2026-08-24 17:59:02 +0530

    Time: 71 ms (11.76%), Space: 153.3 MB (14.11%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-      vector<int> ans;
+          vector<int> ans;
         priority_queue<pair<int, int>> pq; 
         
         for (int i = 0; i < nums.size(); i++) {
