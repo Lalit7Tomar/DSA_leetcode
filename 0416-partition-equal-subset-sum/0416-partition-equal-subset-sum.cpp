8f666073c93f4b0468fc887496cf4c8a460848a0
Lalit7Tomar <lalittomar0612@gmail.com>
2026-09-25 13:21:45 +0530

    Time: 143 ms (43.15%), Space: 139.7 MB (11.85%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,25 +1,28 @@
 class Solution {
 public:
+    bool recursion(vector<int>& nums,int i,int target,vector<vector<int>>& dp){
+           if(target==0){
+        return true;
+     }
+          if(i==0) return (nums[0]==target);
+
+   
+     if(dp[i][target]!=-1) return dp[i][target];
+     bool not_take=recursion(nums,i-1,target,dp);
+     bool take=false;
+     if(nums[i]<=target)take= recursion(nums,i-1,target-nums[i],dp);
+     return dp[i][target]= take || not_take;
+    }
     bool canPartition(vector<int>& nums) {
-        int total = accumulate(nums.begin(), nums.end(), 0);
-        if (total % 2 != 0) return false;
-        int target = total / 2;
-
-        int n = nums.size();
-        vector<vector<bool>> dp(n, vector<bool>(target + 1, false));
-
-        for (int i = 0; i < n; i++) dp[i][0] = true;
-
-        if (nums[0] <= target) dp[0][nums[0]] = true;
-
-        for (int i = 1; i < n; i++) {
-            for (int sum = 1; sum <= target; sum++) {
-                bool notTake = dp[i-1][sum];
-                bool take = false;
-                if (nums[i] <= sum) take = dp[i-1][sum - nums[i]];
-                dp[i][sum] = notTake || take;
-            }
+        int sum=0;
+         for(int i=0;i<nums.size();i++){
+          sum=sum+nums[i];
         }
-        return dp[n-1][target];
+        vector<vector<int>> dp(nums.size(),vector<int>(sum/2+1,-1));
+       
+        if(sum%2!=0){
+            return false;
+        }
+       return recursion(nums,nums.size()-1,sum/2,dp);
     }
 };
